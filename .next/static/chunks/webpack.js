06e64f41495cd1e45a97df01cfbfe66b52eded80
rluizti <rluiz.ti@gmail.com>
2025-11-25 21:00:37 -0300

    add `jfcongig.json` file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b0f011b080f345e"; }
+/******/ 		__webpack_require__.h = function() { return "88f9fd0ead474116"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
